1d1f85a70063aee6ea868632ab3588fb3bd093d4
Ali Nazif Koca <ancakbunubuldum@outlook.com>
2021-11-20 19:04:46 +0300

    feat: add functions for creating window and clearing the screen buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,28 @@
+#include <iostream>
+#include <cassert>
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
+
+
+#define WINDOW_WIDTH    800
+#define WINDOW_HEIGHT   600
+
 int main()
 {
-    
+    assert(glfwInit());
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hickirik", NULL, NULL);
+    assert(window);
+    glfwMakeContextCurrent(window);
+    assert(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress));
+
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
 }

eb9a99061d768944961334405f247d0f4feaafb5
Ali Nazif Koca <ancakbunubuldum@outlook.com>
2021-11-26 08:06:02 +0300

    feat: add triangle and hexagon creation functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
 #include <GLM/vec3.hpp>
+#include <GLM/vec4.hpp>
 #include "ShaderProgram.hpp"
 #include "DefaultDefinitons.hpp"
+#include <random>
+#include <ctime>
 
 #define WINDOW_WIDTH    800
 #define WINDOW_HEIGHT   800
@@ -12,7 +15,18 @@
 Hickirik::Shaders::ShaderProgram* shaderProgram;
 
 uint vertexArrayObject;
-uint vertexBuffer;
+
+int triangleCount;
+
+struct ColoredVertex
+{
+    ColoredVertex(const glm::vec3& pos){
+        position = pos;
+        color = glm::vec4((rand() % 256) / 256.0f, (rand() % 256) / 256.0f, (rand() % 256) / 256.0f , 1.0f);
+    }
+    glm::vec3 position;
+    glm::vec4 color;
+};
 
 glm::vec3 triangleVertices[6]{
     glm::vec3(-1.0f,  1.0f, 0.0f), 
@@ -22,10 +36,75 @@ glm::vec3 triangleVertices[6]{
     glm::vec3( 0.0f,  0.0f, 0.0f), 
     glm::vec3(-1.0f,  0.0f, 0.0f)
 };
+uint createHexagon()
+{
+    ColoredVertex vertexList[7]{
+        ColoredVertex(glm::vec3(-0.25f, 0.433f, 1.0f)),
+        ColoredVertex(glm::vec3(0.25f, 0.433f, 1.0f)),
+        ColoredVertex(glm::vec3(-0.5f, 0.0f, 1.0f)),
+        ColoredVertex(glm::vec3(0.0f, 0.0f, 1.0f)),
+        ColoredVertex(glm::vec3(0.5f, 0.0f, 1.0f)),
+        ColoredVertex(glm::vec3(-0.25f,-0.433f, 1.0f)),
+        ColoredVertex(glm::vec3(0.25f,-0.433f, 1.0f))
+    };
+
+    uint indices[18]{
+        0, 2, 3,
+        0, 1, 3,
+        1, 3, 4,
+        2, 3, 5,
+        3, 5, 6,
+        3, 4, 6
+    };
+
+    uint vao, vertexBuffer, indexBuffer;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexList), &vertexList[0], GL_STATIC_DRAW);
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(ColoredVertex), NULL);
+    glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(ColoredVertex), (const void*)sizeof(glm::vec3));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    triangleCount = 6;
+    return vao;
+}
+
+uint createSquare()
+{
+    uint vao;
+    glm::vec3 vertices[4]{
+        glm::vec3(-0.5f,  0.5f, 1.0f),
+        glm::vec3( 0.5f,  0.5f, 1.0f),
+        glm::vec3(-0.5f, -0.5f, 1.0f),
+        glm::vec3( 0.5f, -0.5f, 1.0f)
+    };
+    uint indexes[6]{
+        0, 1, 2, 1, 2, 3
+    };
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    uint vertexBuffer, indexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), &indexes[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(glm::vec3), NULL);
+    glEnableVertexAttribArray(0);
+    triangleCount = 2;
+    return vao;
+}
 
 void OGL_Start()
 {
-    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    /*
+    uint vertexBuffer;
     shaderProgram = new Hickirik::Shaders::ShaderProgram();
     shaderProgram->AttachShader("./shaders/vertex.glsl", GL_VERTEX_SHADER);
     shaderProgram->AttachShader("./shaders/fragment.glsl", GL_FRAGMENT_SHADER);
@@ -33,11 +112,20 @@ void OGL_Start()
     glGenVertexArrays(1, &vertexArrayObject);
     glBindVertexArray(vertexArrayObject);
     glGenBuffers(1, &vertexBuffer);
+    
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, NULL);
     glEnableVertexAttribArray(0);
+    */
+    srand(time(NULL));
+    shaderProgram = new Hickirik::Shaders::ShaderProgram();
+    shaderProgram->AttachShader("./shaders/vertex.glsl", GL_VERTEX_SHADER);
+    shaderProgram->AttachShader("./shaders/fragment.glsl", GL_FRAGMENT_SHADER);
+    shaderProgram->Link();
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    vertexArrayObject = createHexagon();
 }
 
 void OGL_Frame()
@@ -46,8 +134,7 @@ void OGL_Frame()
     
     shaderProgram->Use();
     glBindVertexArray(vertexArrayObject);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawArrays(GL_TRIANGLES, 3, 6);
+    glDrawElements(GL_TRIANGLES, triangleCount * 3, GL_UNSIGNED_INT, NULL);
 }
 
 void error(int code, const char* error)

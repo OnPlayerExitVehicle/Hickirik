64be4c24b00db4649b68d2333e2ff346107a05ae
Ali Nazif Koca <ancakbunubuldum@outlook.com>
2021-11-27 17:33:10 +0300

    feat(shaders): edit vertex shader for uniform variables

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,9 +3,10 @@
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec4 inColor;
 out vec4 outColor;
+uniform mat3 transformMatrix;
 
 void main()
 {
-    gl_Position = vec4(inPosition, 1.0f);
+    gl_Position = vec4(transformMatrix * inPosition, 1.0f);
     outColor = inColor;
 }

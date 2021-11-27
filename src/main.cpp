9e7eed02c68edc091097663b4a8523a5c523a918
Ali Nazif Koca <ancakbunubuldum@outlook.com>
2021-11-27 17:32:24 +0300

    feat: modify main for Object and GUI classes

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,19 @@
 #include "DefaultDefinitons.hpp"
 #include <random>
 #include <ctime>
+#include <vector>
+#include "Object2D.hpp"
+#include "GUI.hpp"
 
 #define WINDOW_WIDTH    800
 #define WINDOW_HEIGHT   800
 
 Hickirik::Shaders::ShaderProgram* shaderProgram;
 
-uint vertexArrayObject;
-
 int triangleCount;
 
-struct ColoredVertex
-{
-    ColoredVertex(const glm::vec3& pos){
-        position = pos;
-        color = glm::vec4((rand() % 256) / 256.0f, (rand() % 256) / 256.0f, (rand() % 256) / 256.0f , 1.0f);
-    }
-    glm::vec3 position;
-    glm::vec4 color;
-};
+Object2D* object = nullptr;
+GUI* gui;
 
 glm::vec3 triangleVertices[6]{
     glm::vec3(-1.0f,  1.0f, 0.0f), 
@@ -36,70 +30,6 @@ glm::vec3 triangleVertices[6]{
     glm::vec3( 0.0f,  0.0f, 0.0f), 
     glm::vec3(-1.0f,  0.0f, 0.0f)
 };
-uint createHexagon()
-{
-    ColoredVertex vertexList[7]{
-        ColoredVertex(glm::vec3(-0.25f, 0.433f, 1.0f)),
-        ColoredVertex(glm::vec3(0.25f, 0.433f, 1.0f)),
-        ColoredVertex(glm::vec3(-0.5f, 0.0f, 1.0f)),
-        ColoredVertex(glm::vec3(0.0f, 0.0f, 1.0f)),
-        ColoredVertex(glm::vec3(0.5f, 0.0f, 1.0f)),
-        ColoredVertex(glm::vec3(-0.25f,-0.433f, 1.0f)),
-        ColoredVertex(glm::vec3(0.25f,-0.433f, 1.0f))
-    };
-
-    uint indices[18]{
-        0, 2, 3,
-        0, 1, 3,
-        1, 3, 4,
-        2, 3, 5,
-        3, 5, 6,
-        3, 4, 6
-    };
-
-    uint vao, vertexBuffer, indexBuffer;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexList), &vertexList[0], GL_STATIC_DRAW);
-    glGenBuffers(1, &indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(ColoredVertex), NULL);
-    glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(ColoredVertex), (const void*)sizeof(glm::vec3));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    triangleCount = 6;
-    return vao;
-}
-
-uint createSquare()
-{
-    uint vao;
-    glm::vec3 vertices[4]{
-        glm::vec3(-0.5f,  0.5f, 1.0f),
-        glm::vec3( 0.5f,  0.5f, 1.0f),
-        glm::vec3(-0.5f, -0.5f, 1.0f),
-        glm::vec3( 0.5f, -0.5f, 1.0f)
-    };
-    uint indexes[6]{
-        0, 1, 2, 1, 2, 3
-    };
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    uint vertexBuffer, indexBuffer;
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
-    glGenBuffers(1, &indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), &indexes[0], GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(glm::vec3), NULL);
-    glEnableVertexAttribArray(0);
-    triangleCount = 2;
-    return vao;
-}
 
 void OGL_Start()
 {
@@ -120,21 +50,31 @@ void OGL_Start()
     glEnableVertexAttribArray(0);
     */
     srand(time(NULL));
-    shaderProgram = new Hickirik::Shaders::ShaderProgram();
-    shaderProgram->AttachShader("./shaders/vertex.glsl", GL_VERTEX_SHADER);
-    shaderProgram->AttachShader("./shaders/fragment.glsl", GL_FRAGMENT_SHADER);
-    shaderProgram->Link();
-    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-    vertexArrayObject = createHexagon();
+    
+    //object = new Object2D(ObjectType2D::Square);
+    gui = new GUI();
 }
 
 void OGL_Frame()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    gui->Frame();
     
-    shaderProgram->Use();
-    glBindVertexArray(vertexArrayObject);
-    glDrawElements(GL_TRIANGLES, triangleCount * 3, GL_UNSIGNED_INT, NULL);
+    if(object == nullptr)
+    {
+        Object2D::ClearScreen();
+        ObjectType2D type;
+        if(gui->Creator(type))
+        {
+            std::cout << "asd" << std::endl;
+            object = new Object2D(type);
+        }
+    }
+    else{
+        object->Draw();
+        gui->FrameItems(&object->position.x, &object->position.y, &object->rotation, &object->scale.x, &object->scale.y);
+    }
+    
+    gui->Draw();
 }
 
 void error(int code, const char* error)
@@ -158,7 +98,7 @@ int main()
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     //assert();
-
+    gui->Init(window);
     OGL_Start();
 
     while(!glfwWindowShouldClose(window))
